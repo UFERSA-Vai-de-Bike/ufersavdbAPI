3512fad48db901e100715a4ac0f4e044aabd007f
MateusAndrade <mateus.andrade47@outlook.com>
2019-10-01 13:31:16 -0300

    feat(dot-env): applying dot env to db connector and setting dot env example at connector.js

--- app.js
+++ app.js
@@ -2,57 +2,55 @@
 // pegando os pacotes que precisamos
 // ==================================
 
-var express = require('express');
-var cors = require('cors')
+var express = require("express");
+var cors = require("cors");
 var app = express();
-var bodyparser = require('body-parser');
-var morgan = require('morgan');
-
-var jwt =  require('jsonwebtoken'); // usado pra criar, assinar e verificar tokens
-//var config = require('./config'); // pegando o arquivo de configuração
+var bodyparser = require("body-parser");
+var morgan = require("morgan");
+var dotenv = require("dotenv");
+var jwt = require("jsonwebtoken"); // usado pra criar, assinar e verificar tokens
 
 // ==================================
 // configuração =====================
 // ==================================
-// app.set('superSecret', config.secret);
+// importa o dot .env definido no root da aplicacão
+dotenv.config();
+
 // usando bodyparser para podermos pegar info de POST e parâmetros URL
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
-
 // usando morgan para fazer log das requisições no console
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 app.use(cors());
 // ==================================
 // rotas ============================
 // ==================================
-var index = require('./routes/index');
-var users = require('./routes/users');
-var stations = require('./routes/stations');
-var bikes = require('./routes/bikes');
-var transactions = require('./routes/transactions');
+var index = require("./routes/index");
+var users = require("./routes/users");
+var stations = require("./routes/stations");
+var bikes = require("./routes/bikes");
+var transactions = require("./routes/transactions");
 
-
-app.use('/api', index);
-app.use('/api/users', users);
-app.use('/api/stations', stations);
-app.use('/api/bikes', bikes);
-app.use('/api/transactions', transactions);
+app.use("/api", index);
+app.use("/api/users", users);
+app.use("/api/stations", stations);
+app.use("/api/bikes", bikes);
+app.use("/api/transactions", transactions);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 
 // erro de desenvolvimento
-if (app.get('env') === 'development') {
+if (app.get("env") === "development") {
   app.use(function(err, req, res, next) {
-    res.status( err.code || 500 )
-    .json({
-      status: 'error',
+    res.status(err.code || 500).json({
+      status: "error",
       message: err
     });
   });
@@ -60,9 +58,8 @@ if (app.get('env') === 'development') {
 
 // error de produção
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500)
-  .json({
-    status: 'error',
+  res.status(err.status || 500).json({
+    status: "error",
     message: err.message
   });
 });
--- db/connector.js
+++ db/connector.js
@@ -1,25 +1,28 @@
 /**
  * Created by silva on 09/10/17.
  */
-var promise = require('bluebird');
+var promise = require("bluebird");
+
 var options = {
-    promiseLib: promise
+  promiseLib: promise
 };
 
-var pgp = require('pg-promise')(options);
-var connectionString = 'postgresql://postgres:admin@localhost:5432/ufersa_vdb_1';
+var pgp = require("pg-promise")(options);
+
+const { DB_DATABASE, DB_HOST, DB_PASS, DB_PORT, DB_USER } = process.env;
+
+var connectionString = `postgresql://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
 var db = pgp(connectionString);
 
-var queryResult =  {
-    one: 1,
-    many: 2,
-    none: 4,
-    any: 6
+var queryResult = {
+  one: 1,
+  many: 2,
+  none: 4,
+  any: 6
 };
 // add query functions
 
-
 module.exports = {
-    db: db,
-    queryResult: queryResult
+  db: db,
+  queryResult: queryResult
 };
